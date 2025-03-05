3598acb7e2ebc07f4c83ab0129d886f8278f567d
Y79S84M <yostm.dev@gmail.com>
2025-03-05 21:37:20 +0100

    let's start

---
+++ src/main/java/fr/ysaintmartin/tnn/TnnApplication.java
@@ -0,0 +1,13 @@
+package fr.ysaintmartin.tnn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TnnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TnnApplication.class, args);
+	}
+
+}
